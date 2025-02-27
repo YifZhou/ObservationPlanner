6450d68206e39872e4bafaee36a6e14f2dba5b20
YifZhou <zhouyifan1012@gmail.com>
2025-02-27 01:35:59 -0500

    Initial Commit

---
+++ ObservationPlanner/ObservationPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservationPlannerApp.swift
+//  ObservationPlanner
+//
+//  Created by Zhou, Yifan on 2/27/25.
+//
+
+import SwiftUI
+
+@main
+struct ObservationPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
